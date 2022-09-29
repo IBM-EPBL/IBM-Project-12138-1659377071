180190c8657c286d830366415438d4c31c270074
Harish L <harishinlgh112@gmail.com>
2022-09-29 23:45:43 +0530

    Alarm Code

--- Assignments/Assignment 2 - Temperature Alarm/Gokul.py
+++ Assignments/Assignment 2 - Temperature Alarm/Gokul.py
@@ -1,10 +1,12 @@
 import random
 
+def detect_alarm():
+    print("Alarm ringing")
+    
 x = (random.random())*50 #in celcius
 y = (random.random())*100
 
 if(x>40):
     detect_alarm()
 
-def detect_alarm():
-    print("Alarm ringing")
+
--- Assignments/Assignment 2 - Temperature Alarm/Kaiser.py
+++ Assignments/Assignment 2 - Temperature Alarm/Kaiser.py
@@ -1,10 +1,11 @@
 import random
 
-x = (random.random())*50 #in celcius
-y = (random.random())*100
-
-if(x>40):
-    detect_alarm()
-
 def detect_alarm():
     print("Alarm ringing")
+
+
+x = (random.random())*50  # in celcius
+y = (random.random())*100
+
+if (x > 40):
+    detect_alarm()
